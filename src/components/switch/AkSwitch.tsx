83e00c445f0e67ef462a121ab66f655c9b8f06a3
Kodeneko <ladysun.freedom@gmail.com>
2023-10-02 23:30:09 +0200

    [compos] sw base

--- src/components/switch/AkSwitch.tsx
+++ src/components/switch/AkSwitch.tsx
@@ -1,6 +1,42 @@
-const AkSwitch: React.FunctionComponent = () => {
+import {useState} from 'react';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import styles from './styles.module.less';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+type AkSwitchProps = {
+  type: 'round' | 'square';
+  icon: IconProp;
+  labels: [string, string];
+  isRight: boolean;
+  onClick: (isRight: boolean) => void;
+}
+
+const AkSwitch: React.FunctionComponent<AkSwitchProps> = ({
+  type = 'round',
+  icon,
+  labels,
+  isRight,
+  onClick,
+}: AkSwitchProps) => {
+  const [isRightHere, setIsRightHere] = useState<boolean>(isRight);
+  const handleClick: () => void = (): void => {
+    setIsRightHere(!isRightHere);
+    onClick(!isRightHere);
+  };
   return (
-    <div>AkSwitch</div>
+    <div onClick={handleClick}>
+      <div className={styles.iconCont}>
+        <FontAwesomeIcon className={styles.icon} icon={icon} />
+      </div>
+      <div className={styles.sw}>
+        <div className={type === 'square' ? styles.labelsCont : styles.labelsContNone}>
+          <div className={styles.left}>{labels[0]}</div>
+          <div className={styles.right}>{labels[1]}</div>
+        </div>
+        <div className={styles[type]}/> 
+        <div className={styles[`back${type}`]}/>
+      </div>
+    </div>
   );
 };
 
