99336b67eda41245d8eed728d272abdc39ba17f8
Kodeneko <ladysun.freedom@gmail.com>
2023-10-13 20:50:16 +0200

    [screens] frame

--- src/frames/main/AkMainFrame.tsx
+++ src/frames/main/AkMainFrame.tsx
@@ -1,8 +1,35 @@
 import { Outlet } from 'react-router-dom';
+import { AkMainBar } from './components/main-bar';
+import { AkMainBarMobile } from './components/main-bar-mobile';
+import styles from './styles.module.less';
+import { AkFooter } from './components/footer';
 
 export const MenuFrame: React.FunctionComponent = () => {
   return (
-    <Outlet />
+    <div className={styles.frame}>
+      <div className={styles.mainBar}>
+        <AkMainBar
+          onClickCreate={()=>{}}
+          onClickSocial={()=>{}}
+          onClickTheme={()=>{}}
+          onClickLang={()=>{}}
+        />
+      </div>
+      <div className={styles.mainBarMobile}>
+        <AkMainBarMobile
+          onClickCreate={()=>{}}
+          onClickSocial={()=>{}}
+          onClickTheme={()=>{}}
+          onClickLang={()=>{}}
+        />
+      </div>
+      <div className={styles.cont}>
+        <Outlet />
+      </div>
+      <div className={styles.footer}>
+        <AkFooter />
+      </div>
+    </div>
   );
 };
 
