5c89177f2ccdf3d4513b506942f68d239592aeb9
Kodeneko <ladysun.freedom@gmail.com>
2023-10-16 00:22:08 +0200

    [screens] load more + list home

--- src/pages/home/AkHome.tsx
+++ src/pages/home/AkHome.tsx
@@ -1,48 +1,84 @@
 import {useState, useEffect} from 'react';
-import { Animal } from '../../models';
-import { AkSearch } from './components';
+import { Animal, Search } from '../../models';
 import styles from './styles.module.less';
 import { animalServiceMng } from '../../service';
-import { AkCard } from '../../components';
+import { AkButton, AkCard, AkMsgPage, AkOverlay } from '../../components';
 import { useTranslation } from 'react-i18next';
+import { faFrown } from '@fortawesome/free-solid-svg-icons';
+import { AkSearch } from './components';
 
 const AkHome: React.FunctionComponent = () => {
   const {t} = useTranslation();
   const [animalList, setAnimalList] = useState<Animal[]>([]);
-  const [searchStr, setSearchStr] = useState<string>('');
+  const [searchStr, setSearchStr] = useState<Search>({limit: 10, offset: 0, str: ''});
+  const [isLoadMore, setIsLoadMore] = useState<boolean>(true);
 
   const handleSearch: (search: string)=> void = (search: string): void => {
-    animalServiceMng().getBySearch({limit: 10, offset: 0, str: search})
-      .then(setAnimalList);
+    const newSearch: Search = {limit: 10, offset: 0, str: search};
+    getAnimalList(newSearch);
   };
   const handleAll: () => void = (): void => {
-    setSearchStr('');
-    animalServiceMng().getBySearch({limit: 10, offset: 0})
-      .then(setAnimalList);
+    const newSearch: Search = {limit: 10, offset: 0, str: ''};
+    getAnimalList(newSearch);
+  };
+  const hanldeLoadMore: () => void = (): void => {
+    const {offset, limit} = searchStr;
+    const newSearch: Search = {...searchStr, offset: offset + limit};
+    getAnimalList(newSearch);
   };
 
   useEffect(() => {
-    animalServiceMng().getBySearch({limit: 10, offset: 0})
-      .then(setAnimalList);
+    getAnimalList({limit: 10, offset: 0});
   }, []);
+
+  const getAnimalList: (search: Search) => void = (search: Search): void => {
+    setSearchStr(search);
+    animalServiceMng().getBySearch(search)
+      .then((list: Animal[]) => {
+        if(list.length === 0) {
+          setIsLoadMore(false);
+        } else {
+          setAnimalList([...animalList, ...list]);
+        }
+      });
+  };
 
   return (
     <div className={styles.cont}>
       <div className={styles.search}>
         <AkSearch
           placeholder={t('placeH.search')}
-          searchStr={searchStr}
+          searchStr={searchStr.str}
           onSearch={handleSearch}
           onAll={handleAll}
         />
       </div>
-      <div className={styles.list}>
-        {animalList.map((a: Animal) => 
-          <div key={a._id} className={styles.card}>
-            <AkCard animal={a} />
+      {
+        animalList.length === 0 ? 
+          <div className={styles.empty}>
+            <AkMsgPage
+              icon={faFrown}
+              title={t('title.empty')}
+              msg={t('desc.empty')}
+            />
           </div>
-        )}
-      </div>
+          :
+          <div className={styles.list}>
+            {animalList.map((a: Animal) => 
+              <div key={a._id} className={styles.card}>
+                <AkCard animal={a} />
+              </div>
+            )}
+          </div>
+      }
+      {isLoadMore && 
+        <div className={styles.loadMore}>
+          <AkButton 
+            onClick={hanldeLoadMore}
+            title={t('labels.loadMore')}
+          />
+        </div>
+      }
     </div>
   );
 };
